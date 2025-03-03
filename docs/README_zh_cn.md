880cb654d04049b5141d530e3c99da28e5d5a079
HiMeditator <hironin@foxmail.com>
2025-03-03 22:38:56 +0800

    create sidebar

---
+++ docs/README_zh_cn.md
@@ -0,0 +1,18 @@
+<div align="center" >
+<img src="../assets/icon/light-assistant.ico"/>
+</div>
+
+
+<h1 align="center">light-assistant</h1>
+
+<div align="center" >
+<p>Light Assistant 是一个轻量级的 VS Code 代码助手插件.</p></div>
+<div align="center" >
+<p>
+| <a href="../README.md">English</a> | <b>简体中文</b> |
+</p>
+</div>
+
+<hr>
+
+目前，本插件正在初步开发中。
--- src/extension.ts
+++ src/extension.ts
@@ -1,26 +1,18 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
+import { MainViewProvider } from './views/main';
 
-// This method is called when your extension is activated
-// Your extension is activated the very first time the command is executed
+export let extensionPath: string;
+
 export function activate(context: vscode.ExtensionContext) {
-
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "light-assistant" is now active!');
-
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	const disposable = vscode.commands.registerCommand('light-assistant.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from Light Assistant!');
-	});
-
-	context.subscriptions.push(disposable);
+    const mainViewProvider = new MainViewProvider(context.extensionUri);
+    context.subscriptions.push(
+        vscode.window.registerWebviewViewProvider(
+            MainViewProvider.viewType,
+            mainViewProvider,
+            {webviewOptions: {retainContextWhenHidden: true}}
+        )
+    );
 }
 
-// This method is called when your extension is deactivated
+
 export function deactivate() {}
